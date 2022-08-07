e6d4fc7e8517953ef02d5b4e2ea102d7ff0e0717
jan Aki <95918480+jan-Aki@users.noreply.github.com>
2022-08-07 15:54:29 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+
+int main(){
+    system("cls");
+    int r;
+    int c;
+    char m = 'x';
+    int i;
+    int j;
+    int k;
+    char board [3] [3] = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+    };
+
+    for (i = 0; i < 9; i++){
+        if(m == 'x'){
+            printf("x turn\n");
+            scanf("%d%d", &r, &c);
+            board[r][c] = 'x';
+            m = 'o';
+            system("cls");
+        } else {
+            printf("o turn\n");
+            scanf("%d%d", &r, &c);
+            board[r][c] = 'o';
+            m = 'x';
+            system("cls");
+        }
+
+        for (j = 0; j < 3; j++){
+            for (k = 0; k < 3; k++){
+                printf("%c ", board[j][k]);
+            }
+        printf("\n");
+        }
+
+        if(board [0][0] == 'x' && board [0][1] == 'x' && board [0][2] == 'x'){
+                printf("x wins");
+                return 0;
+            }
+        if(board [1][0] == 'x' && board [1][1] == 'x' && board [1][2] == 'x'){
+                printf("x wins");
+                return 0;
+            }
+        if(board [2][0] == 'x' && board [2][1] == 'x' && board [2][2] == 'x'){
+                printf("x wins");
+                return 0;
+            }
+        if(board [0][0] == 'x' && board [1][0] == 'x' && board [2][0] == 'x'){
+                printf("x wins");
+                return 0;
+            }
+        if(board [0][1] == 'x' && board [1][1] == 'x' && board [2][1] == 'x'){
+                printf("x wins");
+                return 0;
+            }
+        if(board [0][2] == 'x' && board [1][2] == 'x' && board [2][2] == 'x'){
+                printf("x wins");
+                return 0;
+            }
+        if(board [0][0] == 'x' && board [1][1] == 'x' && board [2][2] == 'x'){
+                printf("x wins");
+                return 0;
+            }
+        if(board [0][2] == 'x' && board [1][1] == 'x' && board [2][0] == 'x'){
+                printf("x wins");
+                return 0;
+            }
+
+        if( board [0][0] == 'o' && board [0][1] == 'o' && board [0][2] == 'o'){
+                printf("o wins");
+                return 0;
+            }
+        if( board [1][0] == 'o' && board [1][1] == 'o' && board [1][2] == 'o'){
+                printf("o wins");
+                return 0;
+            }
+        if( board [2][0] == 'o' && board [2][1] == 'o' && board [2][2] == 'o'){
+                printf("o wins");
+                return 0;
+            }
+        if( board [0][0] == 'o' && board [1][0] == 'o' && board [2][0] == 'o'){
+                printf("o wins");
+                return 0;
+            }
+        if( board [0][1] == 'o' && board [1][1] == 'o' && board [2][1] == 'o'){
+                printf("o wins");
+                return 0;
+            }
+        if( board [0][2] == 'o' && board [1][2] == 'o' && board [2][2] == 'o'){
+                printf("o wins");
+                return 0;
+            }
+        if( board [0][0] == 'o' && board [1][1] == 'o' && board [2][2] == 'o'){
+                printf("o wins");
+                return 0;
+            }
+        if( board [0][2] == 'o' && board [1][1] == 'o' && board [2][0] == 'o'){
+                printf("o wins");
+                return 0;
+            }
+    }
+printf("it's a draw!");
+return 0;
+}
+
+
+//i am unnecessarily proud of this
+//i feel like there's a way to use a lot fewer variables though
+
+//actually, I'm coming back to this a few days later and i'm pretty proud of it
+//i'm going to try to add the win detection now
+
+//it has win detection now. i'll probably come back and add better detection someday, but today is not that day
+//over half of it is just checking to see if someone won lol
+//i also need to make it so that you can't just overwrite someone's move, but i can't think of an elegant way to do that without making them forfeit or something
